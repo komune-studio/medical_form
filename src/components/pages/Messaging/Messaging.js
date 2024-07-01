8772596f5a55958963a116df265343436162022a
zuhdi <rakazhp@gmail.com>
2024-07-01 18:03:27 +0700

    Created mockup for messaging CRUD

--- src/components/pages/Messaging/Messaging.js
+++ src/components/pages/Messaging/Messaging.js
@@ -1,11 +1,63 @@
-import {Col, Form, Row} from "react-bootstrap";
+import {Form} from "react-bootstrap";
 import React, {useState} from "react";
 import {Card, CardBody, Container} from "reactstrap";
 import Palette from "../../../utils/Palette";
-import {Button} from "antd";
+import {Button as AntButton, Button, Space, Tooltip} from "antd";
+import CustomModal from '../../reusable/CustomModal'
+import CustomTable from "../../reusable/CustomTable";
+import moment from "moment";
+import Iconify from "../../reusable/Iconify";
+
+
+const columns = [
+    {id: 'id', label: 'ID', filter: true, width: '15%'},
+    {id: 'title', label: 'Judul', filter: true, width: '15%'},
+    {id: 'body', label: 'Deskripsi', filter: true, width: '15%'},
+    {id: 'created_by', label: 'Pengirim', filter: true, width: '15%'},
+    {id: 'sent_to', label: 'Penerima', filter: true, width: '15%'},
+    {
+        id: 'created_at', label: 'Tanggal Dibuat', filter: true, width: '15%',
+        render: (row => {
+            return moment(row.created_at).format('DD MMM YYYY HH:mm')
+        })
+    },
+    {
+        id: '', label: '', filter: false,
+        render: ((value) => {
+            return (
+                <>
+                    <Space size="small">
+                        <Tooltip title="Edit">
+                            <AntButton
+                                type={'link'}
+                                style={{color: Palette.MAIN_THEME}}
+                                className={"d-flex align-items-center justify-content-center"}
+                                shape="circle"
+                                icon={<Iconify icon={"material-symbols:edit"}/>}
+
+                            >Edit</AntButton>
+                        </Tooltip>
+                        <Tooltip title={value?.active ? 'Hapus' : 'Restore'}>
+                            {
+                                value?.active ?
+                                    <AntButton type={'link'} style={{color: Palette.MAIN_THEME}} className={"d-flex align-items-center justify-content-center"} shape="circle" icon={<Iconify icon={"material-symbols:delete-outline"}/>}>Hapus</AntButton>
+                                    : <AntButton type={'link'} style={{color: Palette.MAIN_THEME}} className={"d-flex align-items-center justify-content-center"} shape="circle" icon={<Iconify icon={"mdi:restore"}/>}>Restore</AntButton>
+                            }
+
+                        </Tooltip>
+                    </Space>
+                </>
+            )
+
+        })
+    },
+
+]
 
 export default function Messaging() {
     const [form, setForm] = useState({title: '', body: ''})
+    const [isModalOpen, setIsModalOpen] = useState(false)
+    const [dataSource, setDataSource] = useState([])
 
     const CustomField = ({title, onChange, rows}) => {
         return (
@@ -25,20 +77,18 @@ export default function Messaging() {
             <Card style={{background: Palette.BACKGROUND_DARK_GRAY, color: "white"}}
                   className="card-stats mb-4 mb-xl-0">
                 <CardBody>
-                    <Row className={'mb-4'}>
-                        <Col className='mb-3' md={12}>
-                            <div style={{fontWeight: "bold", fontSize: "1.1em"}}>Messaging</div>
-                        </Col>
-                    </Row>
-                    <CustomField title={'Title'} onChange={(e) => setForm({...form, title: e.target.value})}/>
-                    <CustomField title={'Body'} rows={4} as={'textarea'} value={form.body} onChange={(e) => setForm({...form, body: e.target.value})} type="text" placeholder="Body"/>
                     <div style={{display: 'flex', justifyContent: 'right'}}>
                         <Button type={'primary'} size="sm" variant="primary" onClick={onSubmit}>
-                            {'Luncurkan'}
+                            {'Buat Notifikasi'}
                         </Button>
                     </div>
                 </CardBody>
             </Card>
+            <CustomTable showFilter={true} pagination={true} searchText={''} data={dataSource} columns={columns}/>
+            <CustomModal isOpen={false} title={'Create new Notification'} onClose={() => console.log('closed')}>
+                <CustomField title={'Title'} onChange={(e) => setForm({...form, title: e.target.value})}/>
+                <CustomField title={'Body'} rows={4} as={'textarea'} value={form.body} onChange={(e) => setForm({...form, body: e.target.value})} type="text" placeholder="Body"/>
+            </CustomModal>
         </Container>
     )
 }
