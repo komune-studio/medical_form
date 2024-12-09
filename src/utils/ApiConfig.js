dab4e5b3e4a44006848142e33439f27d9805f9fd
wil <adiputrawilbert@gmail.com>
2024-12-09 08:15:54 +0700

    changed api config

--- src/utils/ApiConfig.js
+++ src/utils/ApiConfig.js
@@ -1,4 +1,4 @@
 module.exports = {
-    base_url: "https://barcode-api.komunestudio.com/"
-    // base_url: 'https://api.barcodegokart.com/'
+    // base_url: "https://barcode-api.komunestudio.com/"
+    base_url: 'https://api.barcodegokart.com/'
 };
