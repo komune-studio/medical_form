efe5d416ebb064a097d220c9c07f7bf0d0e6d0e7
TimothyAK <mothyabraham973@gmail.com>
2025-09-02 11:44:50 +0700

    add placeholder util file

---
+++ src/utils/Placeholder.js
@@ -0,0 +1,16 @@
+export default class Placeholder {
+  name = "e.g. John Doe"
+  phone = "e.g. +62 081233445566"
+  email = "e.g. example@email.com"
+  website = "www.example.com"
+  address = "e.g. Jl. Mawar No. 15, RT 005/RW 002, Kel. Mekar, Kec. Kencana, Kota Agung, 12345 or other shorter version"
+  biography = "insert biography here"
+  description = "insert description here"
+  language = "e.g. Indonesia, English, Spanish, French"
+
+  translated = {
+    name: "e.g. John Doe (Translated Version)",
+    biography: "insert biography here (Translated Version)",
+    description: "insert description here (Translated Version)",
+  }
+}
