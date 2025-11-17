29cb912f394d0118edd2503f22afb3c23096193a
wil-pc <adiputrawilbert@gmail.com>
2025-11-17 15:56:02 +0700

    minor change

--- src/utils/ApiConfig.js
+++ src/utils/ApiConfig.js
@@ -1,5 +1,6 @@
 module.exports = {
     base_url: "https://read-indonesia-api.komunestudio.com/"
+    // base_url: 'https://staging-api.readindonesia.id/',
     // base_url: "https://barcode-api.komunestudio.com/"
     
 };
